ba02c8674e2b74e4d72105c4523bd9631d188189
Andres Ordonez <aleshdevelopment@gmail.com>
2022-09-10 16:46:55 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from stories import story
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    prompts = story.prompts
+
+    return render_template('home.html', prompts=prompts)
+
+@app.route('/story')
+def story_page():
+    text = story.generate(request.args)
+
+    return render_template('story.html', text=text)
